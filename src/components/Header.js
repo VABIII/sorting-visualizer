6c3da20169c02a49a8cce73784bfe48d9992104f
VABIII <tbengtson82@gmail.com>
2022-05-18 01:27:44 -0500

    Added the insertionSort.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,9 @@ const Header = props => {
 
     return (
         <div className='header-algos'>
-            <h3 className="selection-algo" onClick={()=> setAlgo('selectionSort')}>
-                Selection Sort
-            </h3>
+            <h3 className="selection-algo" onClick={()=> setAlgo('selectionSort')}>Selection Sort</h3>
             <h3 className="bubble-algo" onClick={()=> setAlgo('bubbleSort')}>Bubble Sort</h3>
+            <h3 className="insertion-algo" onClick={()=> setAlgo('insertionSort')}>Insertion Sort</h3>
         </div>
     );
 };
