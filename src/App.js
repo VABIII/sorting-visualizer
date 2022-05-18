18435e987ecae9faa6f85b1570d8963ae35c17cb
VABIII <tbengtson82@gmail.com>
2022-05-17 23:34:43 -0500

    created generator functions for App.js

--- src/App.js
+++ src/App.js
@@ -29,9 +29,112 @@ class App extends Component {
             arraySteps: steps,
             colorSteps
         })
-
-
     }
+
+    clearTimeouts = () => {
+        this.state.timeouts.forEach(timeout => clearTimeout(timeout));
+        this.setState({
+            timeouts: [],
+        });
+    }
+
+    clearColorKey = () => {
+        let blankKey = new Array(this.state.count).fill(0);
+
+        this.setState({
+            colorKey: blankKey,
+            colorSteps: [blankKey],
+        })
+    }
+
+    generateRandomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    generateRandomArray = () => {
+        this.clearTimeouts();
+        this.clearColorKey();
+        const count = this.state.count;
+        const temp = [];
+
+        for (let i = 0; i < count; i++) temp.push(this.generateRandomNumber(50, 200));
+
+        this.setState({
+            array: temp,
+            arraySteps: [temp],
+            currentStep: 0,
+        },
+            ()=> {
+            this.generateSteps();
+            }
+        );
+    };
+
+    changeArray = (index, value) => {
+        let arr = this.state.array;
+        arr[index] = value;
+        this.setState({
+            array: arr,
+            arraySteps: [arr],
+            currentSteps: 0,
+        },
+            () => {
+            this.generateSteps();
+            }
+        );
+    };
+
+    previousStep = () => {
+        let currentStep = this.state.currentStep;
+        if (currentStep === 0) return;
+        currentStep -= 1;
+        this.setState({
+            currentStep: currentStep,
+            array: this.state.arraySteps[currentStep],
+            colorKey: this.state.colorSteps[currentStep],
+        });
+    };
+
+    nextStep = () => {
+        let currentStep = this.state.currentStep;
+        if (currentStep >= this.state.arraySteps.length - 1) return;
+        currentStep += 1;
+        this.setState({
+            currentStep: currentStep,
+            array: this.state.arraySteps[currentStep],
+            colorKey: this.state.colorSteps[currentStep],
+        });
+    };
+
+    start = () => {
+        let steps = this.state.arraySteps;
+        let colorSteps = this.state.colorSteps;
+
+        this.clearTimeouts();
+
+        let timeouts = [];
+        let i = 0;
+
+        while (i < steps.length - this.state.currentStep) {
+            let timeout = setTimeout(() => {
+                let currentStep = this.state.currentStep;
+                this.setState({
+                    array: steps[currentStep],
+                    colorKey: colorSteps[currentStep],
+                    currentStep: currentStep + 1,
+                });
+                timeouts.push(timeout);
+            }, this.state.delay * i);
+            i++;
+        }
+        this.setState({
+            timeouts
+        })
+    }
+
+
+
+
 
 
 
