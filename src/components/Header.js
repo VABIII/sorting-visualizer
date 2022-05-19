7dc9ebb3f57d71afd016b51cc1a8ba502754bb04
VABIII <tbengtson82@gmail.com>
2022-05-18 21:37:31 -0500

    Added the quickSort header button even though it's not technically a button. It's a h2, actually. No, I mean a h3. I think...

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ const Header = props => {
             <h3 className="selection-algo" onClick={()=> setAlgo('selectionSort')}>Selection Sort</h3>
             <h3 className="bubble-algo" onClick={()=> setAlgo('bubbleSort')}>Bubble Sort</h3>
             <h3 className="insertion-algo" onClick={()=> setAlgo('insertionSort')}>Insertion Sort</h3>
+            <h3 className="insertion-algo" onClick={()=> setAlgo('quickSort')}>Quick Sort</h3>
         </div>
     );
 };
